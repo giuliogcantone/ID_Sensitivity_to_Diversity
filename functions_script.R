c9cd32ccf2862663ad1362b5c08aab3e58479e32
Giulio Giacomo Cantone <ggc23@sussex.ac.uk>
2023-06-07 16:22:44 +0100

    1st

---
+++ functions_script.R
@@ -0,0 +1,11 @@
+###
+
+trueD <- function() {}
+
+
+robustrao::RaoStirling(matrix(rep(c(5,2,2),10),
+                              nrow = 3),
+                       matrix(c(1,0,0,0,1,0,0,0,1),
+                              ncol = 3)) -> RS
+
+1/(1 - RS)
