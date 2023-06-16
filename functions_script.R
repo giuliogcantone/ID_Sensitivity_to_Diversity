5562a3843c099e805336c798f218be0a5a3a9217
Giulio Giacomo Cantone <ggc23@sussex.ac.uk>
2023-06-16 15:53:16 +0100

    up

--- functions_script.R
+++ functions_script.R
@@ -1,18 +1,26 @@
 ###
 
 
-robustrao::RaoStirling(matrix(rep(c(5,2,2),10),
-                              nrow = 3),
-                       matrix(c(1,0,0,0,1,0,0,0,1),
-                              ncol = 3)) -> RS
+robustrao::RaoStirling(c(5.5,0,2),
+                       matrix(c(0,1,1,0,1,1,0,1,1),
+                              ncol = 3)
+                        ) -> RS
+
+RaoStirling
+
+RS * 2
+
+abdiv::simpson(c(5.5,0,2,0,0))
+
+ineq::Gini(c(5.5,0,2), corr = T)
 
 1/(1 - RS)
 
-1 - DescTools::Gini(c(1,2,3))
+1 - DescTools::Gini(c(1,2,3),
+                    unbiased = F)
 
 
-DescTools::GiniSimpson(c(.5,.5))
-
+DescTools::Gini(c(10,10,0))
 abdiv::simpson(c(10,10,10))
 
 abdiv::simpson(c(10,10,10))
